8055806403631f50cf1fe4d387105c4d883426c1
gnave <gilliannave@gmail.com>
2026-04-09 21:41:37 +0100

    Added comments to update_schema to show its use

--- gemini/update_schema.py
+++ gemini/update_schema.py
@@ -1,10 +1,13 @@
 import h5py
 
+# File to update the schema in a hdf5 file, if you decide to change a data structure within it.
+# Edit as appropriate for your files.
+
 filename = 'test.h5'  # <-- CHANGE THIS TO YOUR FILE's NAME
 
 with h5py.File(filename, 'a') as f:
     if '/Levels' in f:
-        # Overwrite the old schema with the new one
+        # Overwrite the old schema with the new one. Modify according to the needed schema
         f['/Levels'].attrs['schema'] = 'key,energy,j_value,parity,lifetime,lifetime_unc_frac,designation'
         print(f"Successfully updated the schema for {filename}!")
     else:
